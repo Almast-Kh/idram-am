5d1ee975f3c252bde64a947493632ebc87a483f9
Almast Khrberdyan <almastkh86@gmail.com>
2021-02-17 17:08:44 +0300

    adding

--- src/test/java/am/itu/qa/transfer/IdramTransfer.java
+++ src/test/java/am/itu/qa/transfer/IdramTransfer.java
@@ -1,11 +1,13 @@
 package am.itu.qa.transfer;
 
+import org.testng.annotations.Test;
+
 import am.itu.qa.idrambase.IdramBaseTest;
 import am.itu.qa.page.transfer.IdramPageTransfer;
 
 public class IdramTransfer extends IdramBaseTest{
 	 
-	
+	@Test
 	public void transfer() throws InterruptedException { 
 		Thread.sleep(5000);
 	IdramPageTransfer trans= new IdramPageTransfer(this.driver);
